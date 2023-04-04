d20de51e2ab7a3425dcc5e05053ef779eacbf6ce
Shivansh Seth <82285859+ShibuShivansh40@users.noreply.github.com>
2023-04-04 14:11:39 +0530

    Accelerometer is sensed and the Color is getting changed for its respected Frame Layout

--- app/src/main/java/com/example/sensormangement_codingblocks/MainActivity.kt
+++ app/src/main/java/com/example/sensormangement_codingblocks/MainActivity.kt
@@ -11,8 +11,10 @@ import android.provider.CalendarContract
 import android.util.Log
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.content.getSystemService
 import java.util.*
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -35,16 +37,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 //            // Here it will change the background color of the FrameLayout for ProxyIndicator whenever tbere is a chhange in the Sensor Value
 //        }
 
-        Log.d("SENSOR_CHANGED_ACCEL" , """
-            ---
-            ax = ${event!!.values[0]}
-            ay = ${event!!.values[1]}
-            az = ${event!!.values[2]}
-            ---
-        """.trimIndent())
+        //        Log.d("SENSOR_CHANGED_ACCEL" , """
+//            ---
+//            ax = ${event!!.values[0]}
+//            ay = ${event!!.values[1]}
+//            az = ${event!!.values[2]}
+//            ---
+//        """.trimIndent())
 
-
+        val bgColor = Color.rgb(
+            accel2Color(event!!.values[0]),
+            accel2Color(event!!.values[1]),
+            accel2Color(event!!.values[2])
+        )
+        val accelIndicator = findViewById<FrameLayout>(R.id.accelIndicator)
+        accelIndicator.setBackgroundColor(bgColor)
     }
+
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,9 +74,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 //                    """${it.name} | ${it.stringType} | ${it.vendor}""".trimIndent())
 //            }
 //        }
-
 //        How to detect change in the Proximity Sensor?
-//        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
 //        How to detect change in the Accelerometer Sensor?
         accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         }
@@ -85,4 +93,5 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)
         super.onPause()
     }
+    private fun accel2Color(accel : Float) :Int = (((accel + 12)/24)*255).roundToInt()
 }
